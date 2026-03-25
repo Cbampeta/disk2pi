b045dadb4dbf058e8c84a703a77dc97e7799280a
Othman Hamza <othmanpce@gmail.com>
2026-03-25 14:46:11 +0100

    feat: compresssion dans pdfutils

--- src/utils/pdf_utils.py
+++ src/utils/pdf_utils.py
@@ -6,3 +6,24 @@ class PDFUtils:
     def __init__(self) -> None:
         self.log = logging.getLogger(__name__)
         self.log.info("Initializing PDFUtils...")
+
+def changer_extension(src, nouvelle_extension):
+    position_du_point = src.index(".")  #trouve l'indice du .
+    nom_sans_extension = src[:position_du_point]
+    dst = nom_sans_extension + "." + nouvelle_extension
+    return dst
+
+
+def pdf_to_txt(src): #seulement les pdf avec du vrai texte 
+    from pypdf import PdfReader  #importe la bibliothèque pour lire les PDFs
+
+    dst = changer_extension(src, "txt")  #crée le nom du fichier de sortie
+    reader = PdfReader(src)   
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    with open(dst, "w", encoding="utf-8") as f:  #ouvre le fichier txt en mode écriture + est adapté pour les caractères spéciaux + accents
+        f.write(text)
+
+    return dst

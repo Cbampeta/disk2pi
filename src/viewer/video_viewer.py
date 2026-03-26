1b38f86d2a639c60213b2ae51200873404b60b77
Cbampeta <cyril.bampeta@telecom-sudparis.eu>
2026-03-26 17:30:30 +0100

    fix: fixing the code for using the main.py

--- src/viewer/video_viewer.py
+++ src/viewer/video_viewer.py
@@ -1,69 +1,82 @@
 import logging
-import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, 
-                             QVBoxLayout, QHBoxLayout, QPushButton, 
-                             QSlider, QFileDialog, QStyle)
+from PyQt6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QFileDialog,
+    QStyle,
+)
 from PyQt6.QtMultimediaWidgets import QVideoWidget
 from PyQt6.QtMultimedia import QMediaPlayer
-from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtCore import QUrl
 
 
-class VideoViewer(QMainWindow):
+class VideoViewer(QWidget):
     def __init__(self, input_file=None):
         super().__init__()
         self.input_file = input_file
         self.log = logging.getLogger(__name__)
         self.log.info("Initializing VideoViewer...")
-        self.input_file = input_file
 
-        self.setGeometry(100, 100, 800, 600)
-
-        #media player and video widget
         self.media_player = QMediaPlayer()
         self.video_widget = QVideoWidget()
         self.media_player.setVideoOutput(self.video_widget)
 
-        #UI elements
         self.open_button = QPushButton("Open Video")
         self.play_button = QPushButton()
-        self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay))
+        self.play_button.setIcon(
+            self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
+        )
         self.stop_button = QPushButton()
-        self.stop_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop))
+        self.stop_button.setIcon(
+            self.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop)
+        )
 
         self.open_button.clicked.connect(self.open_file)
         self.play_button.clicked.connect(self.play_video)
         self.stop_button.clicked.connect(self.stop_video)
-
         self.setup_ui()
 
+        if self.input_file:
+            self.load_video(input_file)
+            self.play_video()
+
+    def load_video(self, file_name):
+        video_url = QUrl.fromLocalFile(file_name)
+        self.media_player.setSource(video_url)
+
     def open_file(self):
-        file_name, _ = QFileDialog.getOpenFileName(self, "Open Video File", "", "Video Files (*.mp4 *.avi *.mkv *.mov)")
+        file_name, _ = QFileDialog.getOpenFileName(
+            self, "Open Video File", "", "Video Files (*.mp4 *.avi *.mkv *.mov)"
+        )
 
         if file_name:
-            video_url = QUrl.fromLocalFile(file_name)
-            self.media_player.setSource(video_url)
-            self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPause))
+            self.load_video(file_name)
             self.media_player.play()
+            self.play_button.setIcon(
+                self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPause)
+            )
 
-    def play_video(self):
-        """Toggle between play and pause."""
+    def play_video(self) -> None:
         if self.media_player.isPlaying():
             self.media_player.pause()
-            self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay))
+            self.play_button.setIcon(
+                self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
+            )
         else:
             self.media_player.play()
-            self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPause))
+            self.play_button.setIcon(
+                self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPause)
+            )
 
     def stop_video(self):
-        """Stop the video playback."""
         self.media_player.stop()
-        self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay))
-    
+        self.play_button.setIcon(
+            self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
+        )
+
     def setup_ui(self):
-
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
         layout = QVBoxLayout()
 
         controls_layout = QHBoxLayout()
@@ -74,10 +87,4 @@ class VideoViewer(QMainWindow):
         layout.addWidget(self.video_widget)
         layout.addLayout(controls_layout)
 
-        central_widget.setLayout(layout)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    player = VideoViewer()
-    player.show()
-    sys.exit(app.exec())
+        self.setLayout(layout)

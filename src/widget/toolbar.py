33e9f852b5243c762b1fde48a0c04f5cc05284fb
Cbampeta <cyril.bampeta@telecom-sudparis.eu>
2026-03-30 10:06:18 +0200

    feat: adding a pdf viewer

--- src/widget/toolbar.py
+++ src/widget/toolbar.py
@@ -2,19 +2,11 @@ from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QToolBar, QWidgetAction
 
 
-def create_toolbar(mainWindow) -> None:
-    # We need to bind the toolbar button click event to the main window, so we can handle them
-    mainWindow.toolbar_button_clicked = in_toolbar_button_clicked.__get__(mainWindow)
+def create_toolbar(main_window) -> None:
+    toolbar = QToolBar("My main toolbar", main_window)
+    main_window.addToolBar(toolbar)
 
-    toolbar = QToolBar("My main toolbar")
-    mainWindow.addToolBar(toolbar)
-
-    # Create a button in the toolbar and connect it to the click event
-    button_action = QAction("Button", mainWindow)
+    button_action = QAction("Button", main_window)
     button_action.setStatusTip("This is a button in the toolbar")
-    button_action.triggered.connect(mainWindow.toolbar_button_clicked)
+    button_action.triggered.connect(main_window.in_toolbar_button_clicked)
     toolbar.addAction(button_action)
-
-
-def in_toolbar_button_clicked(mainWindow, s) -> None:
-    print("Toolbar button clicked!", s)

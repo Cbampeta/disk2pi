38b0687c7438a903405e037c7fca80b2948227da
Cbampeta <cyril.bampeta@telecom-sudparis.eu>
2026-04-01 14:20:58 +0200

    feat: adding some feature like menu bar

--- src/overlay/image_overlay.py
+++ src/overlay/image_overlay.py
@@ -2,7 +2,7 @@ import logging
 
 
 class ImageOverlay:
-    def __init__(self) -> None:
+    def __init__(self, overlay) -> None:
         self.log = logging.getLogger(__name__)
         self.log.info("Initializing ImageOverlay...")
         pass

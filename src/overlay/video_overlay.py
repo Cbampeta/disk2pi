a2f5a4134f3c8a3072f527e06185d342bbcd4d8f
Hetnalgorythm <hetnalawakbar@gmail.com>
2026-04-06 13:28:17 +0200

    e

--- src/overlay/video_overlay.py
+++ src/overlay/video_overlay.py
@@ -7,19 +7,19 @@ from PyQt6.QtMultimediaWidgets import QVideoWidget
 from PyQt6.QtMultimedia import QMediaPlayer
 from PyQt6.QtCore import Qt, QUrl
 
-class VideoOverlay:
-    def __init__(self, input_file) -> None:
+class VideoOverlay(QMainWindow):
+    def __init__(self, input_file=None) -> None:
         super().__init__()
         self.input_file = input_file
         self.log = logging.getLogger(__name__)
         self.log.info("Initializing VideoOverlay...")
-
+        self.media_player = QMediaPlayer()
 
         self.open_button = QPushButton("Open Video")
         self.play_button = QPushButton()
-        self.play_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay))
+        self.play_button.setIcon(QApplication.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay))
         self.stop_button = QPushButton()
-        self.stop_button.setIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop))
+        self.stop_button.setIcon(QApplication.style().standardIcon(QStyle.StandardPixmap.SP_MediaStop))
 
         self.open_button.clicked.connect(self.open_file)
         self.play_button.clicked.connect(self.play_video)
@@ -52,8 +52,10 @@ class VideoOverlay:
     
     def setup_ui(self):
 
+        print("3")
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+        print("4")
 
         layout = QVBoxLayout()
 
@@ -62,13 +64,11 @@ class VideoOverlay:
         controls_layout.addWidget(self.play_button)
         controls_layout.addWidget(self.stop_button)
 
-        layout.addWidget(self.video_widget)
         layout.addLayout(controls_layout)
-
         central_widget.setLayout(layout)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    player = VideoViewer()
-    player.show()
+    overlay = VideoOverlay()
+    overlay.show()
     sys.exit(app.exec())

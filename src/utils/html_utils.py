6046e2d0d4f98c5458565a0ea7f945a825ac3d32
othmantsp <othman.hamza@telecom-sudparis.eu>
2026-04-24 13:05:48 +0200

    Update html_utils.py

--- src/utils/html_utils.py
+++ src/utils/html_utils.py
@@ -1,4 +1,19 @@
 from .utils import Utils
 
 class HTMLUTILS:
+  def __init__(self) -> None:
+        self.log = logging.getLogger(__name__)
+        self.log.info("Initializing HTMLUTILS...")
+
+    def html_to_pdf(source):    
+
+    output = Utils.output_file_name("html_to_pdf", "pdf")  # crée le nom du fichier de sortie
+    Utils.creer_fichier(output)                       # crée le fichier vide
+
+    with open(source, "r", encoding="utf-8") as f:  # lit le contenu html
+        contenu = f.read()
+
+    pdfkit.from_string(contenu, output)  # convertit le html en pdf
+    
+    return output
   

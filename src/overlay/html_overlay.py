79e8fb3448dbaf075a4d2839911a92c165045e94
othmantsp <othman.hamza@telecom-sudparis.eu>
2026-04-24 13:28:56 +0200

    Update html_overlay.py

--- src/overlay/html_overlay.py
+++ src/overlay/html_overlay.py
@@ -1,1 +1,40 @@
+import logging
 
+from utils import PDFUtils
+import threading
+
+
+class HTMLOverlay:
+  
+    def __init__(self, overlay) -> None:
+        self.input_file = overlay.input_file
+        self.menu = overlay.menu
+        self.log = logging.getLogger(__name__)
+        self.log.info("Initializing HTMLOverlay...")
+
+        self.html_to_pdf_running = False
+
+        self.init_conversion_panel()
+
+
+    def init_conversion_panel(self):
+        menu_conversion = self.menu.addMenu("&Conversion")
+        menu_conversion.addAction(
+            "Convert HTML to PDF", lambda: self.convert_html_to_pdf()
+        )
+
+    def convert_html_to_pdf(self):
+        if self.html_to_pdf_running:
+            self.log.warning("HTML to PDF conversion is already running.")
+            return
+        self.log.info("Starting HTML to PDF conversion in a separate thread...")
+        self.html_to_pdf_running = True
+
+        def run_conversion():
+            try:
+                PDFUtils.html_to_pdf(self.input_file)
+            finally:
+                self.html_to_pdf_running = False
+                self.log.info("HTML to PDF conversion finished.")
+
+        threading.Thread(target=run_conversion).start()

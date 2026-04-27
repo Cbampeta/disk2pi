88fd86ddeb9a9db929c07b30f525433b3c0527ba
othmantsp <othman.hamza@telecom-sudparis.eu>
2026-04-27 19:38:10 +0200

    Update html_utils.py
    rajout de html to txt

--- src/utils/html_utils.py
+++ src/utils/html_utils.py
@@ -2,7 +2,7 @@ from .utils import Utils
 import pdfkit
 from PyPDF2 import PdfReader
 import logging
-
+from bs4 import BeautifulSoup
 
 class HTMLUtils:
     def __init__(self) -> None:
@@ -21,5 +21,22 @@ class HTMLUtils:
             contenu = f.read()
 
         pdfkit.from_string(contenu, output)  # convertit le html en pdf
+
+        return output
+        
+    @staticmethod    
+    def html_to_txt(source):
+        output = Utils.output_file_name("html_to_txt", "txt")
+        Utils.creer_fichier(output)
+
+        with open(source, "r", encoding="utf-8") as f:
+            contenu = f.read() #on lit le html                        
+
+        #traitement : on enlève les balises html
+        soup = BeautifulSoup(contenu, "html.parser")
+        texte = soup.get_text()
+
+        with open(output, "w", encoding="utf-8") as f:
+            f.write(texte) #on écrit le texte extrait
 
         return output

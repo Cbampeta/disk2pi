6d93bb94911a91354be76ec4cf20a623242e1d0b
othmantsp <othman.hamza@telecom-sudparis.eu>
2026-04-28 19:43:44 +0200

    Update xlsv_overlay.py

--- src/overlay/xlsv_overlay.py
+++ src/overlay/xlsv_overlay.py
@@ -1,1 +1,12 @@
+class XLSXOverlay:
 
+    def __init__(self, overlay) -> None:
+        self.input_file = overlay.input_file
+        self.menu = overlay.menu
+        self.log = logging.getLogger(__name__)
+        self.log.info("Initializing XLSXOverlay...")
+        self.init_conversion_panel()
+
+    def init_conversion_panel(self):
+        menu_conversion = self.menu.addMenu("&Conversion")
+        

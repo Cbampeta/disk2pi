f4f782cbbdfb2d605f060a49e399d04032d8dcba
Cbampeta <cyril.bampeta@telecom-sudparis.eu>
2026-05-18 21:43:46 +0200

    fix: troù function

--- src/utils/video_utils.py
+++ src/utils/video_utils.py
@@ -10,20 +10,21 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QPushButton,
     QFileDialog,
-    QLabel
+    QLabel,
 )
 
 from PySide6.QtCore import QUrl, Qt
+
 
 class VideoUtils:
     def __init__(self, input_file=None) -> None:
         self.log = logging.getLogger(__name__)
         self.log.info("Initializing VideoUtils...")
-        
+
     # def video_trim(self):
     #     self.trim_slider = QRangeSlider(Qt.Orientation.Horizontal)
     #     self.trim_slider.setRange(0,0)
-        
+
     #     self.trim_slider.valueChanged.connect(on_change)
 
     #     label = QLabel()
@@ -33,25 +34,26 @@ class VideoUtils:
     #         if not self.user_is_seeking:
     #             self.trim_slider.setValue(position)
     @staticmethod
-    def on_change(input_file,start_ms,end_ms):
-
+    def on_change(input_file, start_ms, end_ms):
         start_sec = start_ms / 1000
         end_sec = end_ms / 1000
 
         input_stream = ffmpeg.input(input_file)
 
-        video = input_stream.video.filter('trim', ss=start_sec)
-        audio = input_stream.audio.filter('atrim', ss=start_sec)
+        video = input_stream.video.filter("trim", start=start_sec, end=end_sec).filter(
+            "setpts", "PTS-STARTPTS"
+        )
+
+        audio = input_stream.audio.filter("atrim", start=start_sec, end=end_sec).filter(
+            "asetpts", "PTS-STARTPTS"
+        )
 
         output = Utils.output_file_name("trim", "mp4")
 
         (
-            ffmpeg
-            .output(video, audio, output, to=end_sec, c='copy')
+            ffmpeg.output(video, audio, output, vcodec="libx264", acodec="aac")
+            .overwrite_output()
             .run()
         )
 
         return output
-            
-
-

e5d1454b3d623ecc081a5424340e715e01099944
Cbampeta <cyril.bampeta@telecom-sudparis.eu>
2026-05-26 10:51:41 +0200

    fix: adding some other things for path ffmpeg

--- src/config/config.py
+++ src/config/config.py
@@ -10,19 +10,21 @@ def get_ffmpeg_path() -> str:
     system = platform.system().lower()
 
     if system == "windows":
-        bundled = resource_path("ffmpeg\\ffmpeg.exe")
+        bundled = resource_path("vendor\\windows\\ffmpeg.exe")
     elif system == "linux":
-        bundled = resource_path("ffmpeg/ffmpeg")
+        bundled = resource_path("vendor/linux/ffmpeg")
     else:
         raise RuntimeError(f"Système non supporté : {platform.system()}")
-
+    print(f"Recherche de FFmpeg dans : {bundled}")
     if bundled.exists():
         if system == "linux":
             bundled.chmod(bundled.stat().st_mode | 0o111)
+        print(f"FFmpeg trouvé dans le bundle : {bundled}")
         return str(bundled)
 
     fallback = shutil.which("ffmpeg")
     if fallback:
+        print(f"FFmpeg trouvé dans le système : {fallback}")
         return fallback
 
     raise RuntimeError("FFmpeg introuvable.")
